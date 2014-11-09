a05d3b7949549f70fda9a71a40882c4f0ed0150d
Ken McLeod <admin@therinken.com>
2014-11-09 01:45:02 -0600

    setting up intro files

--- js/app.js
+++ js/app.js
@@ -9,7 +9,13 @@
             {url: "./bower_components/jquery/dist/jquery.min.js"},
             {url: "./bower_components/lodash/dist/lodash.min.js"},
             {url: "./bower_components/backbone/backbone.js"},
-            {url: "./bower_components/pathjs/path.min.js"}
+            {url: "./bower_components/backbone.localStorage/backbone.localStorage.js"},
+            {url: "./bower_components/pathjs/path.min.js"},
+            {url: "./js/models/todo.js"},
+            {url: "./js/collections/todos.js"},
+            {url: "./js/views/todos.js"},
+            {url: "./js/views/app.js"},
+            {url: "./js/routers/router.js"}
         ).then(function(){
             _.templateSettings.interpolate = /{([\s\S]+?)}/g;
 
